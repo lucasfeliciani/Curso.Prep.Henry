7b130aba5853395aa421da3da9c0ea6b9b872ba1
lucasfeliciani <lucasfeliciani@gmail.com>
2022-09-02 10:40:17 -0300

    pimer homework terminada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -242,7 +242,20 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.length < 2 ){
+    switch (letra){
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        return "Es vocal";
+          break
+        return "Dato incorrecto";
+    } 
+  }
+    return "Dato incorrecto";   
+
 }
 
 
